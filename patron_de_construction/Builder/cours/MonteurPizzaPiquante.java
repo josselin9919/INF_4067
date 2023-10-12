6d68df057317d8747e9f03efa79bd2d3a1baa6ba
josselin9919 <josselinngakou@gmail.com>
2023-10-12 19:48:57 +0100

    creation du monteur congret "MonteurPizzaPiquante"

--- patron_de_construction/Builder/cours/MonteurPizzaPiquante.java
+++ patron_de_construction/Builder/cours/MonteurPizzaPiquante.java
@@ -1,5 +1,16 @@
 package patron_de_construction.Builder.cours;
 
 public class MonteurPizzaPiquante extends MonteurPizza{
-   
+    public void monterPate() {pizza.setPate("feuilletée ");
+        
+    }
+
+    public void monterSauce() {
+        pizza.setSauce("piquante");
+       
+    }
+
+    public void monterGarniture() {
+       pizza.setGarniture("pepperoni+salami");
+    }
 }
