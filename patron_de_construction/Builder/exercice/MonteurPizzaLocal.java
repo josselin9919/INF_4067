76963082714a3db568221f962f0eb96a4c1dbd95
josselin9919 <josselinngakou@gmail.com>
2023-10-12 20:25:37 +0100

    creation de "MonteurPizzaLocal"

---
+++ patron_de_construction/Builder/exercice/MonteurPizzaLocal.java
@@ -0,0 +1,16 @@
+package patron_de_construction.Builder.exercice;
+
+public class MonteurPizzaLocal extends MonteurPizza{
+    public void monterPate() {pizza.setPate("feuille de macabo ");
+        
+    }
+
+    public void monterSauce() {
+        pizza.setSauce("sauce jaune");
+       
+    }
+
+    public void monterGarniture() {
+       pizza.setGarniture("escargots");
+    }
+}
