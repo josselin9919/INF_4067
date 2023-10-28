58f17fc881d50f8469886dd7095f8be882395f54
josselin9919 <josselinngakou@gmail.com>
2023-10-28 13:38:50 +0100

    creation de DocumentPdf

---
+++ patrons_de_structuration/patron_Adapter/exemple1/DocumentPdf.java
@@ -0,0 +1,21 @@
+package patrons_de_structuration.patron_Adapter.exemple1;
+
+public class DocumentPdf implements Document{
+
+    
+    public int setContenu() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setContenu'");
+    }
+
+    public int dessine() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dessine'");
+    }
+
+    public int imprime() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'imprime'");
+    }
+    
+}
