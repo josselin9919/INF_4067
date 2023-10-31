c360577246e1f4285c5e9c65f39cc4b7dc3c078c
josselin9919 <josselinngakou@gmail.com>
2023-10-31 08:57:00 +0100

    creation de l'adaptateur

---
+++ patrons_de_structuration/patron_Adapter/exemple3/AdaptRectangle.java
@@ -0,0 +1,19 @@
+package patrons_de_structuration.patron_Adapter.exemple3;
+
+public class AdaptRectangle implements ICarre{
+    public Rectangle rectangle;
+    public float cote;
+    
+    public float perimetre() {
+        rectangle.SetLarg(cote);
+        rectangle.SetLong(cote);
+        return rectangle.perimetre();
+    }
+
+    public float aire() {
+        rectangle.SetLarg(cote);
+        rectangle.SetLong(cote);
+        return rectangle.aire();
+    }
+    
+}
--- patrons_de_structuration/patron_Adapter/exemple3/Rectangle.java
+++ patrons_de_structuration/patron_Adapter/exemple3/Rectangle.java
@@ -3,10 +3,17 @@ package patrons_de_structuration.patron_Adapter.exemple3;
 public class Rectangle {
     private float longueur;
     private float largeur;
+    
     public float perimetre(){
         return (longueur+largeur)*2;
     }
     public float aire(){
         return longueur*largeur;
+    }
+    public void SetLong(float lon){
+        longueur=lon;
+    }
+    public void SetLarg(float larg){
+        largeur=larg;
     }
 }
