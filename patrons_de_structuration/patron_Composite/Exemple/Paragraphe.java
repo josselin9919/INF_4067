445475eb26c0aa8a9f16624ccd7585009f0f9d13
josselin9919 <josselinngakou@gmail.com>
2023-11-14 10:12:50 +0100

    creation de texte et la classe paragraphe

---
+++ patrons_de_structuration/patron_Composite/Exemple/Paragraphe.java
@@ -0,0 +1,10 @@
+package patrons_de_structuration.patron_Composite.Exemple;
+
+public class Paragraphe implements Texte {
+
+    
+    public int longueur() {
+        return 20;
+    }
+    
+}
---
+++ patrons_de_structuration/patron_Composite/Exemple/Texte.java
@@ -0,0 +1,9 @@
+package patrons_de_structuration.patron_Composite.Exemple;
+
+public interface Texte {
+
+    String titre=null;
+    String preambule=null;
+    public int longueur();
+    
+}
